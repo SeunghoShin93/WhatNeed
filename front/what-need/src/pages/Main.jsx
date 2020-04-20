01bac276b735be059cb94337bed085b02604b8ea
Bae SangHwa <sanghwa1026@gmail.com>
2020-04-20 16:53:15 +0900

    front webcam capture

---
+++ front/what-need/src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WebcamCapture from "./WebcamCapture";
+
+const Main = props =>{
+    return(
+        <div>
+            <WebcamCapture>
+
+            </WebcamCapture>
+        </div>
+    );
+};
+
+export default Main;
